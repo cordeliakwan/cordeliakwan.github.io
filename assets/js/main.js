648d812226f927b22bf0de17a7b88e407ccce095
Cordelia Kwan <cordelia.kwan@gmail.com>
2021-03-26 02:55:43 -0400

    basic gsap animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
-
-/* common */
-function vh(v) {
-    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    return (v * h) / 100;
-  }
-  
-  function vw(v) {
-    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    return (v * w) / 100;
-  }
-
-  /* work */
-  function toggleUIUX() {
-    
-  }
+  gsap.from(".fade-up-1", {
+    opacity: 0, 
+    y: 80, 
+    duration: 1
+  });
+  gsap.from(".fade-up-2", {
+    opacity: 0, 
+    y: 80, 
+    duration: 1,
+    delay: 0.3
+  });
+  gsap.from(".fade-up-3", {
+    opacity: 0, 
+    y: 80, 
+    duration: 1,
+    delay: 0.6
+  });
+  gsap.from(".fade-up-4", {
+    opacity: 0, 
+    y: 80, 
+    duration: 1,
+    delay: 0.9
+  });
+  gsap.from(".fade-up-5", {
+    opacity: 0, 
+    y: 80, 
+    duration: 1,
+    delay: 1.2
+  });
